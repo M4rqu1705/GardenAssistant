d53cba370f2c74b92f60911a3bcb608d10aa90b1
M4rc05 <marcospesante11@gmail.com>
2017-11-29 04:40:55 -0800

    November 29, 2017

---
+++ Code/2017-11-25/After HTML/HTML, CSS, JavaScript/mainMenu/mainScript.js
@@ -0,0 +1,7 @@
+bodyElement = document.getElementsByTagName("BODY")[0];
+bodyElement.style.height = window.innerHeight+"px";
+bodyElement.style.width = window.innerHeight*(9/16)+"px";
+
+gridLayoutButtonElement = document.getElementById("gridLayoutButton");
+gridLayoutButtonElement.style.width = (window.innerHeight*(9/16))*0.85+"px";
+gridLayoutButtonElement.style.width = (gridLayoutButtonElement.width*(20/116))+"px";
