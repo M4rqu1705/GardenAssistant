884d57b29126a958f69dc7c71320d57c200e2142
M4rqu1705 <marcospesante11@gmail.com>
2017-11-29 22:54:09 -0400

    November 29, 2017 Completed HTML, CSS and JavaScript layout

---
+++ Code/2017-11-25/After HTML/HTML, CSS, JavaScript/lunarPhases/lunarPhasesScript.js
@@ -0,0 +1,13 @@
+var bodyElement = document.getElementsByTagName("body")[0];
+bodyElement.style.height = window.innerHeight+"px";
+bodyElement.style.width = window.innerHeight*(9/16)+"px";
+
+var lunarPhasesHeaderElement = document.getElementById("lunarPhasesHeader");
+lunarPhasesHeaderElement.style.width = bodyElement.style.width+"px";
+lunarPhasesHeaderElement.style.height = (window.innerHeight*.08)+"px";
+
+var lunarPhasesBackButtonElement = document.getElementById("lunarPhasesBackButton");
+lunarPhasesBackButtonElement.style.height = (window.innerHeight*.05)+"px";
+lunarPhasesBackButtonElement.style.width = (window.innerHeight*.05)+"px";
+lunarPhasesBackButtonElement.style.marginTop = (window.innerHeight*.015)+"px";
+lunarPhasesBackButtonElement.style.marginLeft = lunarPhasesBackButtonElement.style.marginTop;
--- Code/2017-11-25/After HTML/HTML, CSS, JavaScript/mainMenu/mainScript.js
+++ Code/2017-11-25/After HTML/HTML, CSS, JavaScript/mainMenu/mainScript.js
@@ -1,7 +1,23 @@
-bodyElement = document.getElementsByTagName("BODY")[0];
+var bodyElement = document.getElementsByTagName("body")[0];
 bodyElement.style.height = window.innerHeight+"px";
 bodyElement.style.width = window.innerHeight*(9/16)+"px";
 
-gridLayoutButtonElement = document.getElementById("gridLayoutButton");
-gridLayoutButtonElement.style.width = (window.innerHeight*(9/16))*0.85+"px";
-gridLayoutButtonElement.style.width = (gridLayoutButtonElement.width*(20/116))+"px";
+
+var mainMenuButtonElements = [document.getElementById("mainMenuGridLayoutButton"),
+							document.getElementById("mainMenuJournalButton"),
+							document.getElementById("mainMenuPlantLogButton"),
+							document.getElementById("mainMenuLunarPhaseButton"),
+							document.getElementById("mainMenuSettingsButton")];
+							
+for(C=0; C<mainMenuButtonElements.length; C++){
+	
+	mainMenuButtonElements[C].style.width = (window.innerHeight*(9/16))*0.80+"px";
+	mainMenuButtonElements[C].style.height = (mainMenuButtonElements[C].style.width*(20/116))+"px";
+	
+	/*mainMenuButtonElements[C].onmousedown = function(){
+		mainMenuButtonElements[C].style.opacity = "0.5";
+	}
+	mainMenuButtonElements[C].onmouseup = function(){
+		mainMenuButtonElements[C].style.opacity = "1";
+	}*/
+}
