1856fe83d597600382fc4249948b655ed20407f1
M4rqu1705 <marcospesante11@gmail.com>
2018-01-25 06:09:47 -0400

    January 25, 2018

--- WebVersion/scripts/gridLayoutScript.js
+++ WebVersion/scripts/gridLayoutScript.js
@@ -8,13 +8,20 @@ $("body").css({height:appHeight, width:appWidth});
 $("#gridLayoutHeader").css({height:(appHeight*0.08), width:appWidth});
 $("#gridLayoutBackButton").css({height:(appHeight*0.05), width:(appHeight*0.05), "margin-top":(appHeight*.015), "margin-left":(appHeight*.015)});
 $("#gridLayoutSaveButton").css({height:(appHeight*0.05), width:(appHeight*0.05), right:( ((window.innerWidth-appWidth)/2)+(appWidth*.11)), "margin-top":(appHeight*.015), "margin-right":(appHeight*.015)});
-$("#gridLayoutSettingsButton").css({height:(appHeight*0.05), width:(appHeight*0.05) ,right:((window.innerWidth- appWidth)/2 + appWidth*0.025), "margin-top":(appHeight*.015), "margin-left":(appHeight*.015)});
+$("#gridLayoutSettingsButton").css({height:(appHeight*0.05), width:(appHeight*0.05) ,right:((window.innerWidth- appWidth)/2 + appWidth*0.025), "margin-top":(appHeight*.015), "margin-left":(appHeight*.015)}).on("click", function(){
+		$("#gridLayoutSettings").dialog("open");
+});
 $("#gridLayoutTextInput").css({height:(appHeight*0.08), width:(appWidth*0.65), "margin-left":(appWidth*0.14)});
 $("#gridLayoutTextInput > input[type='text']").css({height:(appHeight*0.047), width:(appWidth*0.45), "margin-top":(appHeight*0.015), padding:0});
 $("#gridLayoutTextInput > input[type='submit']").css({height:(appHeight*0.047), width:(appWidth*0.15), "margin-top":(appHeight*0.015), padding:0});
 
-//$("#imageSelectionAlert").css({height:appWidth, width:appWidth, margin:"auto", top:(appHeight*0.08)});
-$("#gridLayoutIconContainer").css({"width":appWidth, "height":(appHeight*0.9), "padding-top":(appHeight*0.8), "padding-left":(appWidth*0.05), "position":"absolute", top:(appHeight*0.08), "z-index":1});
+$("#gridLayoutIconContainer").css({"width":appWidth, "height":(appHeight*0.9), padding:0, "margin-top":(appHeight*0.8), "padding-left":(appWidth*0.05), "position":"absolute", top:(appHeight*0.08), "z-index":1}).droppable({
+				drop: function (e, ui) {
+					if(!((/original/).test($(ui.draggable).attr("class")))){
+						$(ui.draggable).remove();
+					}
+				}
+			});;
 
 $(".gridLayoutIcon").css({"width":appWidth*0.09, "z-index":"5"}).draggable({
 	helper:'clone',
@@ -24,7 +31,23 @@ $(".gridLayoutIcon").css({"width":appWidth*0.09, "z-index":"5"}).draggable({
 	}
 });
 
+//Dialogs
  $("#imageSelectionAlert").dialog({
+    autoOpen: false,
+    height: (appWidth*0.9),
+    width: (appWidth*0.9),
+	modal: true,
+	draggable:false,
+	closeOnEscape: false,
+	position:{ my: "top", at: "top+25%", of: "#gridLayoutBody" },
+	buttons: {
+            "": function () {
+                $(this).dialog("close");
+            }
+        }
+}).css({height:(appWidth*0.1), width:(appWidth*0.875), padding: ((appHeight*0.025) + "px 0px 0px " + (appWidth*0.06) + "px	"), margin:"auto"});
+
+$("#gridLayoutSettings").dialog({
     autoOpen: false,
     height: (appWidth*0.9),
     width: (appWidth*0.9),
@@ -33,6 +56,8 @@ $(".gridLayoutIcon").css({"width":appWidth*0.09, "z-index":"5"}).draggable({
 	closeOnEscape: false,
 	position:{ my: "top", at: "top-25%", of: "#gridLayoutBody" }
 }).css({height:(appWidth*0.25), width:(appWidth*0.875), padding: ((appHeight*0.025) + "px 0px 0px " + (appWidth*0.06) + "px	"), margin:"auto"});
+
+
 
 $(".imageSelectionAlertImage").css({height:(appWidth*0.175), width:(appWidth*0.175), padding:0, margin:"0"});
 
@@ -59,10 +84,7 @@ function createGrid(){
 		for(var D = 0; D<10; D++){
 			var secondDiv = document.createElement("div");
 
-			$(secondDiv).attr({
-				"id":String("box"+"_"+C+"_"+D),
-				"class":"box"
-			}).css({
+			$(secondDiv).attr({ "id":String("box"+"_"+C+"_"+D), "class":"box"}).css({
 				width:appWidth*0.1,
 				height:appWidth*0.1,
 				position:"absolute",
@@ -79,7 +101,14 @@ function createGrid(){
 							helper:"original",
 							//grid: [appWidth*0.1, appWidth*0.1],
 							stack:"true"
-						}).css({"margin":"auto"}).removeClass("original");
+						}).css({
+							margin:"auto",
+							padding:0,
+							top:0,
+							left:0,
+							bottom:0,
+							right:0
+						}).removeClass("original");
 					}
 					else{
 						$(ui.draggable).appendTo($(this)).draggable({
@@ -87,6 +116,7 @@ function createGrid(){
 							//grid: [appWidth*0.1, appWidth*0.1],
 						}).css({
 							margin:"auto",
+							padding:0,
 							top:0,
 							left:0,
 							bottom:0,
@@ -94,6 +124,8 @@ function createGrid(){
 						});
 					}
 				}
+			}).on("click", function(){
+					$("#imageSelectionAlert").dialog("open");
 			});
 
 			secondDiv.addEventListener("click", function(){
@@ -117,7 +149,6 @@ function createGrid(){
 }
 
 function backgroundImageAlert(){
-	$("#imageSelectionAlert").dialog("open");
 }
 
 function changeSquareBackground(image){
